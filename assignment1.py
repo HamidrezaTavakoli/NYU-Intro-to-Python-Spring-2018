42536edc90f280d96751b362586676043ed0f59e
HamidrezaTavakoli <36176988+HamidrezaTavakoli@users.noreply.github.com>
2018-02-14 20:50:48 -0500

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+
+# constants
+TWEET_MAX_LEN_COUNT = 280
+
+# global variables
+finalTweet = ""
+orderToPerform = "y"
+
+# helper functions
+def getTweet():
+    remaining = TWEET_MAX_LEN_COUNT - len(finalTweet)
+    tweet = input("type your tweet (characters left " + str(remaining)+ "): ")
+    return tweet
+
+
+def dealWihtEnteredTweet(tweetedValue):
+    global finalTweet
+    finalTweet = finalTweet + tweetedValue + " "
+    decision = input("This is your tweet so far \"" + finalTweet + "\". Would you like to add to it? (y/n):" )
+    return decision
+
+# main function
+while len(finalTweet) < TWEET_MAX_LEN_COUNT :
+    if orderToPerform == "y":
+        enteredTweet = getTweet()
+        if len(enteredTweet) > TWEET_MAX_LEN_COUNT:
+            print("Tweet is too long. Max characters allowed is " + str(TWEET_MAX_LEN_COUNT) + ".")
+            break
+        else :
+            orderToPerform = dealWihtEnteredTweet(enteredTweet)
+    else :
+        print("Posting your tweet")
+        print(finalTweet)
+        break
